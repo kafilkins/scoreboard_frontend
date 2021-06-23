e16f5d6f6e16df4db5d76ff76485e2cb0470e763
Keith Filkins <kafilkins@me.com>
2021-06-23 18:41:22 -0400

    Adding additional packages that are needed for the project, saving them to my package.json file.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
   <React.StrictMode>
